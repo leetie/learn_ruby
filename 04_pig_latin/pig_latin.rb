c26e6a7056576f2e9746f5801b462598c65ef6c1
Jesse Parsons <savvyjesse@aol.com>
2020-02-10 14:10:02 -0500

    completed exercises

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,65 @@
 #write your code here
+
+#def translate word
+  # vowelArray = ["a", "e", "i", "o", "u", "y"]
+  # wordArray = word.split("")
+  # output = ""
+  # vowelArray.each do |vowel| 
+  #   if wordArray[0] == vowel
+  #     output = word + "ay"
+  #     return output
+  #   end
+  #   break
+  # end
+
+#   firstLetter = wordArray.slice!(0, 1)
+#   puts firstLetter
+#   wordArray.push(firstLetter)
+#   puts wordArray
+#   wordArray.join() + "ay"
+# end
+#square
+def translate_word(word)
+  first_letter = word[0].downcase
+  
+  if word[0..1] == "qu"
+    return word[2..-1] + word[0..1] + "ay"
+  end
+  if word[0] == "s" && word[1..2] == "qu"
+    
+    return word[3..-1] + word[0] + word[1..2] + "ay"
+  end
+
+  if ["a", "e", "i", "o", "u"].include?(first_letter)
+    #translate word that starts with vowel
+    "#{word}ay"
+  else
+    #translate word that starts with consonant(s)
+    consonants = []
+    consonants << word[0]
+
+      if["a", "e", "i", "o", "u"].include?(word[1]) == false
+        consonants << word[1]
+
+          if["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+
+          end
+
+      end
+        "#{word[consonants.length..-1] + consonants.join + "ay"}"
+  end
+end
+
+def translate(string)
+  a = string.split(" ")
+  b = a.map {|word| translate_word(word)}
+  b.join(" ")
+end
+  
+puts translate_word("square")
+  
+  
+  
+
+  
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
-class Book
-# write your code here
+class Book 
+  attr_accessor :title
+
+  def title=(str)
+    str = str.capitalize
+    words = str.split(" ")
+
+    small_words = ["and", "or", "the", "if", "a", "in", "of", "an"]
+    
+    words.each do |word|
+      small_words.include?(word) ? next :
+      word[0] = word[0].upcase
+    end
+
+    @title = words.join(" ")
+  end
 end
+
+     
+    
+
+
