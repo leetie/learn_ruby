eb8d7b746d2f6551cb43311375c348a3d424cdec
Jesse Parsons <savvyjesse@aol.com>
2020-02-09 19:21:39 -0500

    committed completed exersizes

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
 #write your code here
+def add num1, num2
+  num1 + num2
+end
+
+def subtract num1, num2
+  num1 - num2
+end
+
+def sum array
+  total = 0
+  for i in array
+    total += i
+  end
+  if total == [] || total == 0
+    return 0
+  end
+  total
+end
+
+def multiply num1, num2
+  num1 * num2
+end
+
+def power num1, num2
+  num1 ** num2
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,54 @@
 #write your code here
+def echo str
+  str
+end
+
+def shout str
+  str.upcase
+end
+
+def repeat str, num = 2
+  if num == 3
+    return str + " " + str + " " + str
+  end
+  str + " " + str
+
+end
+
+def start_of_word word, num = 1
+  #substr
+  word[0, num]
+end
+
+def first_word str 
+  str = str.split
+  return str[0]
+end
+
+def titleize str
+ str = str.gsub(/\w+/) { |word| word.capitalize }
+ strArray = str.split
+
+#  for i in strArray.size
+#   i = 1
+#   if strArray[i] == "And" || strArray[i] == "The" 
+#     return strArray[i].downcase
+#   end
+#   i += 1
+
+  i = 1
+  #ignores first word in str.array
+  until i == strArray.length do
+    if strArray[i] == "And" || strArray[i] == "The" || strArray[i] == "Over"
+      strArray[i] = strArray[i].downcase
+    end
+    i += 1
+  end
+
+
+
+ #puts str
+ return strArray.join(" ")
+end
+
+puts titleize "the game is the best"
